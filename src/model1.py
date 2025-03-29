e4d9746ee19afa6e89576267e385dc3ee9930578
mbds <60073138+muhammadbagusd@users.noreply.github.com>
2025-03-30 00:11:26 +0100

    Update model1.py

--- src/model1.py
+++ src/model1.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fluprodia import FluidPropertyDiagram
 
-def create_connections(network=None, charging_mode=True, temp=None):
+def create_connections_discharge(network=None, charging_mode=True, Tsto_in=None, Tsto_out=None, Tenv=None):
     # Create Brayton Cycle Network
     # Ensure temp is provided for non-looped operation
-    if temp is None:
+    if Tsto_out is None:
         raise ValueError("Temperature must be provided.")
 
     # HP Components
@@ -44,65 +44,66 @@ def create_connections(network=None, charging_mode=True, temp=None):
     # ------------ Create Connections for Charging Mode -------------
     if charging_mode:
         # Heat Pump Cycle Connections (Closed Loop with CycleCloser)
-        c1 = Connection(cc, 'out1', ev, 'in1', label='1')
-        c2 = Connection(ev, 'out1', cp, 'in1', label='2')
-        c3 = Connection(cp, 'out1', hx1, 'in2', label='3')
-        c4 = Connection(hx1, 'out2', hx2, 'in2', label='4')
-        c5 = Connection(hx2, 'out2', cd, 'in2', label='5')
-        c6 = Connection(cd, 'out2', va, 'in1', label='6')
+        c1 = Connection(cc, 'out1', ev, 'in2', label='1')
+        c2 = Connection(ev, 'out2', cp, 'in1', label='2')
+        c3 = Connection(cp, 'out1', hx1, 'in1', label='3')
+        c4 = Connection(hx1, 'out1', hx2, 'in1', label='4')
+        c5 = Connection(hx2, 'out1', cd, 'in1', label='5')
+        c6 = Connection(cd, 'out1', va, 'in1', label='6')
         c7 = Connection(va, 'out1', cc, 'in1', label='7')
 
         network.add_conns(c1, c2, c3, c4, c5, c6, c7)
 
         # Alternative connection
-        c11 = Connection(aso, 'out1', ev, 'in2', label='11')
-        c12 = Connection(ev, 'out2', asi, 'in1', label='12')
+        c11 = Connection(aso, 'out1', ev, 'in1', label='11')
+        c12 = Connection(ev, 'out1', asi, 'in1', label='12')
         # Hot and Cold Storage Interfaces
-        c21 = Connection(sso, "out1", cd, "in1", label='21')
-        c22 = Connection(cd, "out1", hx2, "in1", label="22")
-        c23 = Connection(hx2, "out1", hx1, "in1", label="23")
-        c24 = Connection(hx1, "out1", ssi, "in1", label="24")
+        c21 = Connection(sso, "out1", cd, "in2", label='21')
+        c22 = Connection(cd, "out2", hx2, "in2", label="22")
+        c23 = Connection(hx2, "out2", hx1, "in2", label="23")
+        c24 = Connection(hx1, "out2", ssi, "in1", label="24")
 
         network.add_conns(c11, c12, c21, c22, c23, c24)
         
         # Set attributes for the heat pump cycle
         # Set up for model with storage temperature
-        hx1.set_attr(pr1=0.98, pr2=0.98, ttd_u=5) # ttd_l=5) # uncomment for model with environment temp setting
-        hx2.set_attr(pr1=0.98, pr2=0.98) # ttd_u=5) 
-        ev.set_attr(pr1=0.98, pr2=0.98, ttd_l=5)
-        cp.set_attr(eta_s=0.8)
-        cd.set_attr(pr1=1, pr2=1, ttd_l=20)
+        hx1.set_attr(pr2=1, pr1=1, kA=1e5) # ttd_l=5) # uncomment for model with environment temp setting
+        hx2.set_attr(pr1=1, ttd_l=5) # ttd_u=5) 
+        ev.set_attr(pr1=1, ttd_u=5) #ttd_l = out1-in2, ttd_u = in1 - out2
+        cp.set_attr(P=5e6)
+        cd.set_attr(pr1=1, pr2=1, ttd_l=5)
+        # va.set_attr(pr=0.1)
         # Storage connection
-        c24.set_attr(m=10, T=temp, fluid={"water": 1}) # T=40 for model with environment temp setting
-        c21.set_attr(T=50)
+        c24.set_attr(T=Tsto_out, x=0, fluid={"water": 1}) # T=40 for model with environment temp setting
+        # c23.set_attr(T=120)
+        c21.set_attr(x=0, T=Tsto_in)
         # main connection
-        c2.set_attr(x=1, fluid={'NH3': 1})
+        c1.set_attr(p=5)
+        c2.set_attr(m=10, x=1, fluid={'NH3': 1})
         # c7.set_attr(p=1)
-        c3.set_attr(p=25) # comment for Model with evironment temp setting
-        # c5.set_attr(T=60)
-        c6.set_attr(x=0)
+        # c3.set_attr(p=10) # comment for Model with evironment temp setting
+        # c4.set_attr(T=190)
+        # c5.set_attr(T=185)
+        # c7.set_attr(T=-10)
+        # c6.set_attr(x=0)
         # evaporator connection
         c12.set_attr(m=10)
-        c11.set_attr(T=10, p=1, fluid={'water': 1})
+        c11.set_attr(T=Tenv, p=1, fluid={'water': 1})
         
-        # # Parameter for environment temp
-        # # Set attributes for the heat pump cycle
-        # hx1.set_attr(pr1=0.98, pr2=0.98) # ttd_u=5
-        # hx2.set_attr(pr1=0.98, pr2=0.98)
-        # ev.set_attr(pr1=0.98, pr2=0.98, Q=1e6)
-        # cp.set_attr(eta_s=0.85)
-        # cd.set_attr(pr1=0.98, pr2=0.98, ttd_l=5) # ttd_l=5
-        # # Storage connection
-        # c21.set_attr(T=50, m=10, fluid={"water": 1})  
-        # c24.set_attr(T=160)
-        # # main connection
-        # c2.set_attr(p=1, x=1, fluid={'NH3': 1})
-        # c3.set_attr(p=15)
-        # c6.set_attr(x=0)
-        # # evaporator connection
-        # c12.set_attr(m=10)
-        # c11.set_attr(T=temp, p=1, fluid={'water': 1})
-        
+         # Generator setup
+        ep_hp = Bus("Produkt")
+        ep_hp.add_comps(
+            {"comp": ssi, "char": 0.98, "base": "component"}, # bus itu buat yg masuk ke system, component buat yg keluar
+            {"comp": sso, "char": 0.98, "base": "bus"},
+        ) 
+        ef_hp = Bus('Fuel')
+        ef_hp.add_comps(
+            {'comp': cp, 'base': 'bus'},
+        )
+        el_hp = Bus('Loss')
+        el_hp.add_comps({'comp': aso, 'base': 'bus'}, {'comp': asi}) # bus itu buat yg masuk ke system, component buat yg keluar
+
+        network.add_busses(ep_hp, ef_hp, el_hp)
         network.solve(mode='design')
         network.print_results()
 
@@ -131,43 +132,51 @@ def create_connections(network=None, charging_mode=True, temp=None):
         network.add_conns(c11, c12, c21, c22, c23, c24)
 
         # Set attributes for Rankine cycle
-        cd.set_attr(pr1=0.98, pr2=0.98)
-        tb.set_attr(eta_s=0.8)
-        p.set_attr(eta_s=0.8, P=1e3)
-        ph.set_attr(pr1=0.98, pr2=0.98, ttd_l=20)
-        ev.set_attr(pr1=0.98, pr2=0.98, ttd_u=15)
-        sh.set_attr(pr1=0.98, pr2=0.98)
+        cd.set_attr(pr1=0.98, pr2=0.98) 
+        tb.set_attr(eta_s=0.9)
+        p.set_attr(eta_s=0.95)
+        ph.set_attr( pr2=0.98)
+        ev.set_attr(pr1=0.98, pr2=0.98)
+        sh.set_attr(pr1=0.98, pr2=1, ttd_l=50)
 
         # Set Condenser connection
-        c11.set_attr(p=1, T=30, fluid={'water': 1}) 
+        c11.set_attr(p=1, T=Tenv, fluid={'water': 1}) 
         c12.set_attr(m=10)
         # Set Storage connection
-        c21.set_attr(T=temp, p=10, m=10, fluid={'water': 1})
+        c21.set_attr(T=Tsto_out, x=0, fluid={'water': 1})
+        c24.set_attr(T=Tsto_in, x=0)
         # Set Main cycle connection
-        # c2.set_attr(p=1)
+        # c2.set_attr(x=0.9)
+        c3.set_attr(T=80)
         #c3.set_attr(m=10, fluid={'ethanol': 1})
         c6.set_attr(x=1)  # Evaporate at 70°C
-        c7.set_attr(Td_bp=10)
-        c4.set_attr(fluid={'ethanol': 1})
-        c5.set_attr(x=0)
+        c7.set_attr(Td_bp=5)
+        c4.set_attr(m=10, fluid={'ethanol': 1})
+        c5.set_attr(x=0.9)
 
         # Generator setup
         gen = Bus("generator")
         gen.add_comps(
             {"comp": tb, "char": 0.98, "base": "component"},
             {"comp": p, "char": 0.98, "base": "bus"},
+        ) 
+        ef_orc = Bus('Fuel')
+        ef_orc.add_comps(
+            {'comp': sso, 'base': 'bus'},
+            {'comp': ssi, 'base': 'component'},
         )
-        network.add_busses(gen)
+        el_orc = Bus('Loss')
+        el_orc.add_comps({'comp': aso, 'base': 'bus'}, {'comp': asi})
+        
+        network.add_busses(gen, ef_orc, el_orc)
+        
         network.solve(mode='design')
-        nw.print_results()
-    return network, cd, hx1, hx2, cp, c22, c23, c4, c5
-    # return network, gen, ph, ev, sh
+        network.print_results()
+    # return network, cd, hx1, hx2, cp, ep_hp, ef_hp, el_hp
+    return network, gen, ph, ev, sh, ef_orc, el_orc
 
-nw = Network(p_unit='bar', T_unit='C', h_unit='kJ / kg')
+nw_1 = Network(p_unit='bar', T_unit='C', h_unit='kJ / kg')
 
 # model with storage output temperature
-network, cd, hx1, hx2, cp, c22, c23, c4, c5  = create_connections(network=nw, charging_mode=True, temp=130)
-# network, gen, ph, ev, sh = create_connections(network=nw, charging_mode=False, temp=140)
-
-# uncomment for model with environment temp setting
-# network = create_connections(network=nw, charging_mode=True, temp=10)
+# network, cd, hx1, hx2, cp, ep_hp, ef_hp, el_hp  = create_connections_discharge(network=nw_1, charging_mode=True, Tsto_in=70, Tsto_out=170, Tenv=10)
+network, gen, ph, ev, sh, ef_orc, el_orc = create_connections_discharge(network=nw_1, charging_mode=False, Tsto_in=70, Tsto_out=170, Tenv=10)
